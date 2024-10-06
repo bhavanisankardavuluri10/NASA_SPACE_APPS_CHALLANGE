949988ec1e1edc5537856222ee2a9598cdfa167a
Anshuman Samanta <132367367+Anshuman22coder@users.noreply.github.com>
2024-10-06 05:44:33 +0530

    Update globe.js

--- globe.js
+++ globe.js
@@ -92,7 +92,7 @@ function checkAnswers(questions, quizContainer, teamName) {
 
 // Function to send the updated score to the server
 function updateTeamScore(teamName, score) {
-    fetch('/updateScore', {
+    fetch('https://backend-finalle.onrender.com/user/updateScore', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
